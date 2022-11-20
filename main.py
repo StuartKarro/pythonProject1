8a9f457d1cdf9b940d034c8bd8793a96f213f9bf
StuartKarro <110871602+StuartKarro@users.noreply.github.com>
2022-11-20 19:37:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ xy = ''
 atkXY = ''
 pc_valido = ''
 player_valido = ''
-pontuacao_player = 0
-pontuacao_computador = 0
+pontuacao_player = 5
+pontuacao_computador = 5
 jogada = 0
 posicoes_player = []
 posicoes_computador = []
@@ -16,14 +16,27 @@ def tabuleiro(linha, coluna,):
         tab.append(coluna * [0])
     return tab
 
+def mostrartabj():
+    print("\nTabuleiro do Jogador")
+    for c in range(5):
+        print(jogador[c])
+
+
+def mostrartabc():
+    print("\nTabuleiro do Computador")
+    for d in range(5):
+        print(ocultoComputador[d])
+    print('------------------------------------')
+
 
 jogador = tabuleiro(5, 10)
 computador = tabuleiro(5, 10)
+ocultoComputador = tabuleiro(5, 10)
 
 
 for i in range(5):
     while player_valido != 'valido':
-        xy = str(input(f'Digite a posição do {i+1}º do barco (x, y): '))
+        xy = str(input(f'Digite a posição do {i}º do barco, para x de 0 à 4, para y de 0 à 9 (x, y): '))
         if xy not in posicoes_player:
             jogador[int(xy[0])][int(xy[1])] = 1
             posicoes_player.append(xy)
@@ -38,31 +51,44 @@ for i in range(5):
     player_valido = ''
     pc_valido = ''
 
-
-for c in range(5):
-    print(jogador[c])
-print('------------------------------------')
-for d in range(5):
-    print(computador[d])
+mostrartabc()
+mostrartabj()
 
 
 while True:
-    if pontuacao_player == 5 or pontuacao_computador == 5:
+    if pontuacao_player == 0 or pontuacao_computador == 0:
         break
     else:
         if jogada % 2 == 0: #player ataca
-            atkXY = str(input('Digite o a posição de deseja atacar (x, y): '))
+            atkXY = str(input('Digite o a posição de deseja atacar, para x de 0 à 4, para y de 0 à 9(x, y): '))
             if computador[int(atkXY[0])][int(atkXY[1])] == 1:
+                ocultoComputador[int(atkXY[0])][int(atkXY[1])] = 'x'
+                pontuacao_computador -= 1
                 print('Voce acertou!')
-                pontuacao_player += 1
+                mostrartabc()
+                print("Embarcações Restantes",pontuacao_player)
+                mostrartabj()
+                print("Embarcações Restantes",pontuacao_computador)
+
             else:
                 print('Voce errou!')
             jogada += 1
         else: #pc ataca
             atkXY = str(randint(0, 4)) + str(randint(0, 9))
             if jogador[int(atkXY[0])][int(atkXY[1])] == 1:
+                jogador[int(atkXY[0])][int(atkXY[1])] = 'x'
+                pontuacao_player -= 1
                 print('Computador acertou!')
-                pontuacao_computador += 1
+                mostrartabc()
+                print("Embarcações Restantes", pontuacao_player)
+                mostrartabj()
+                print("Embarcações Restantes", pontuacao_computador)
             else:
                 print('Computador errou!')
             jogada += 1
+
+if pontuacao_player == 0:
+    print("Parabéns, você afundou todas as embarcações do inimigo")
+else:
+    print("Você perdeu, o Computador derrubou todas as suas embarcações")
+print("Jogo desenvolvido por Stuart Correa, Nicolas Lamback e Caroline Sales")
